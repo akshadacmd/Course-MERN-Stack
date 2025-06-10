225a1f8c6bd10ee48b390c4534938cdab2300ed6
akshadacmd <akshadap46@gmail.com>
2025-06-10 21:01:51 +0530

    d

---
+++ 2 Month/javaScript/1index.js
@@ -0,0 +1,23 @@
+//variables
+let name = "Akshada"
+const secondName = "Jaywant"
+var surname = "Patil"
+
+//functions
+function addNumbers(a,b){
+    console.log(a+b)
+}
+
+//show msg
+console.log("My name is Akshada")
+
+//Array 
+const firstArray = ["akshu","Hari","Om"]
+
+// Objects
+const firstObject = {
+    name: "akshu"
+} 
+
+//  string
+const firstString = "akshu"
