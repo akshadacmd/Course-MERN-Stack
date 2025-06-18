c57dc6c46f01810b26614a92229aa5f3b7036bd5
akshadacmd <akshadap46@gmail.com>
2025-06-18 19:53:18 +0530

    d

---
+++ 2 Month/CRUD/frontend/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Student from "./pages/Student"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import'./app.css';
+function App() {
+  
+
+  return (
+    <>
+     
+
+      <Student/>
+    </>
+  )
+}
+
+export default App
