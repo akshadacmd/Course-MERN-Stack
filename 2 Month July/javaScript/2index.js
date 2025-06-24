06d9d4e8733a89c8d8584c7d66d3c18310b11a40
akshadacmd <akshadap46@gmail.com>
2025-06-24 14:38:46 +0530

    rename folders

---
+++ 2 Month July/javaScript/2index.js
@@ -0,0 +1,11 @@
+const fristName = "akshada"
+const secondName = "Jaywant"
+const lastName = "Patil"
+
+function writefullName(fristName,secondName,lastName){
+    const fullName = fristName + secondName + lastName;
+    console.log(fullName);
+
+}
+
+console.log(writefullName(fristName,secondName,lastName))c
